a56dbc8c2fe3acae46967d24c0e355cdd6838149
george-mcintyre <george@level-n.com>
2021-12-06 14:05:33 +0100

    Try different epics version

--- src/main/java/edu/stanford/slac/aida/client/impl/PvAcccessRequestExecutor.java
+++ src/main/java/edu/stanford/slac/aida/client/impl/PvAcccessRequestExecutor.java
@@ -1,16 +1,9 @@
 package edu.stanford.slac.aida.client.impl;
 
-import org.epics.pvaccess.ClientFactory;
-import org.epics.pvaccess.client.ChannelProvider;
-import org.epics.pvaccess.client.ChannelProviderFactory;
-import org.epics.pvaccess.client.ChannelProviderRegistryFactory;
 import org.epics.pvaccess.client.rpc.RPCClientImpl;
 import org.epics.pvaccess.server.rpc.RPCRequestException;
 import org.epics.pvdata.pv.PVStructure;
 
-import java.util.Arrays;
-
-import static org.epics.ca.ClientFactory.PROVIDER_NAME;
 import static org.epics.pvdata.pv.Status.StatusType.ERROR;
 
 public class PvAcccessRequestExecutor {
@@ -25,7 +18,6 @@ public class PvAcccessRequestExecutor {
             if ( client != null ) {
                 client.destroy();
             }
-//            ClientFactory.stop();
         }
     }
 }
