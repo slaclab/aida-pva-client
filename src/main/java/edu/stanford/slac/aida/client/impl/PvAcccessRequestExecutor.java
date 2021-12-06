0b29c577da7e7b5a187ccc7be8c1baf60beb2f3d
george-mcintyre <george@level-n.com>
2021-12-06 05:05:51 +0100

    Different PvAccess pattern

--- src/main/java/edu/stanford/slac/aida/client/impl/PvAcccessRequestExecutor.java
+++ src/main/java/edu/stanford/slac/aida/client/impl/PvAcccessRequestExecutor.java
@@ -1,6 +1,8 @@
 package edu.stanford.slac.aida.client.impl;
 
 import org.epics.pvaccess.ClientFactory;
+import org.epics.pvaccess.client.ChannelProvider;
+import org.epics.pvaccess.client.ChannelProviderRegistryFactory;
 import org.epics.pvaccess.client.rpc.RPCClientImpl;
 import org.epics.pvaccess.server.rpc.RPCRequestException;
 import org.epics.pvdata.pv.PVStructure;
@@ -10,6 +12,10 @@ import static org.epics.pvdata.pv.Status.StatusType.ERROR;
 public class PvAcccessRequestExecutor {
     public static PVStructure executeRequest(String channelName, PVStructure request) throws RPCRequestException {
         try {
+            ChannelProvider cp = ChannelProviderRegistryFactory.getChannelProviderRegistry().getProvider("pva");
+            if ( cp == null ) {
+                throw new RPCRequestException(ERROR, "No pva provider registered");
+            }
             RPCClientImpl client = new RPCClientImpl(channelName);
             PVStructure result = client.request(request, 3.0);
             return result;
