70b0b7041825a41f4d18bebd00d9371933d2c8a0
george-mcintyre <george@level-n.com>
2021-12-06 01:03:32 +0100

    refactor pvaClient impl

--- src/main/java/edu/stanford/slac/aida/client/impl/PvaClientRequestExecutor.java
+++ src/main/java/edu/stanford/slac/aida/client/impl/PvaClientRequestExecutor.java
@@ -11,15 +11,6 @@ import static org.epics.pvdata.pv.Status.StatusType.ERROR;
 public class PvaClientRequestExecutor {
     public static PVStructure executeRequest(String channelName, PVStructure request) throws RPCRequestException {
         try {
-/*
-            PvaClientRPC pvaClientRPC = PvaClient.get("pva").channel(channelName).createRPC();
-            pvaClientRPC.issueConnect();
-            Status status = pvaClientRPC.waitConnect();
-            if (!status.isOK()) {
-                throw new RPCRequestException(status.getType(), status.getMessage());
-            }
-            return pvaClientRPC.request(request);
-*/
             return PvaClient.get("pva").channel(channelName).rpc(request);
         } catch (Exception e) {
             throw new RPCRequestException(ERROR, e.getMessage());
