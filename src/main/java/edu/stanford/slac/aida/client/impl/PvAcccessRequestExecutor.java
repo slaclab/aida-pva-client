a560358ba58b23fc1d92e96eb7a25a0539cec616
george-mcintyre <george@level-n.com>
2021-12-06 04:42:17 +0100

    Different PvAccess pattern

--- src/main/java/edu/stanford/slac/aida/client/impl/PvAcccessRequestExecutor.java
+++ src/main/java/edu/stanford/slac/aida/client/impl/PvAcccessRequestExecutor.java
@@ -10,14 +10,14 @@ import static org.epics.pvdata.pv.Status.StatusType.ERROR;
 public class PvAcccessRequestExecutor {
     public static PVStructure executeRequest(String channelName, PVStructure request) throws RPCRequestException {
         try {
-            ClientFactory.start();
             RPCClientImpl client = new RPCClientImpl(channelName);
             PVStructure result = client.request(request, 3.0);
             client.destroy();
-            ClientFactory.stop();
             return result;
         } catch (Exception e) {
             throw new RPCRequestException(ERROR, e.getMessage(), e);
+        } finally {
+            ClientFactory.stop();
         }
     }
 }
