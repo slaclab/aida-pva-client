4c805c7c219cca25b364159e24576615085bfdfb
george-mcintyre <george@level-n.com>
2021-12-06 13:08:40 +0100

    Try different epics version

--- src/main/java/edu/stanford/slac/aida/client/impl/PvAcccessRequestExecutor.java
+++ src/main/java/edu/stanford/slac/aida/client/impl/PvAcccessRequestExecutor.java
@@ -12,24 +12,42 @@ import static org.epics.pvdata.pv.Status.StatusType.ERROR;
 public class PvAcccessRequestExecutor {
     public static PVStructure executeRequest(String channelName, PVStructure request) throws RPCRequestException {
         RPCClientImpl client = null;
+        ChannelProvider cp = null;
         try {
             ClientFactory.start();
-            ChannelProvider cp = ChannelProviderRegistryFactory.getChannelProviderRegistry().getProvider("pva");
+            cp = ChannelProviderRegistryFactory.getChannelProviderRegistry().getProvider("pva");
             if ( cp == null ) {
-                System.out.println("Can't find PVA channel provider");
-                ClientFactory.stop();
-                ClientFactory.start();
-                System.out.println("Reset!!");
+                System.out.println("Can't find PVA channel provider: 1");
             }
             client = new RPCClientImpl(channelName);
+            cp = ChannelProviderRegistryFactory.getChannelProviderRegistry().getProvider("pva");
+            if ( cp == null ) {
+                System.out.println("Can't find PVA channel provider: 2");
+            }
             return client.request(request, 3.0);
         } catch (Exception e) {
+            cp = ChannelProviderRegistryFactory.getChannelProviderRegistry().getProvider("pva");
+            if ( cp == null ) {
+                System.out.println("Can't find PVA channel provider: 3");
+            }
             throw new RPCRequestException(ERROR, e.getMessage(), e);
         } finally {
+            cp = ChannelProviderRegistryFactory.getChannelProviderRegistry().getProvider("pva");
+            if ( cp == null ) {
+                System.out.println("Can't find PVA channel provider: 4");
+            }
             if ( client != null ) {
                 client.destroy();
+                cp = ChannelProviderRegistryFactory.getChannelProviderRegistry().getProvider("pva");
+                if ( cp == null ) {
+                    System.out.println("Can't find PVA channel provider: 5");
+                }
             }
             ClientFactory.stop();
+            cp = ChannelProviderRegistryFactory.getChannelProviderRegistry().getProvider("pva");
+            if ( cp == null ) {
+                System.out.println("Can't find PVA channel provider: 6");
+            }
         }
     }
 }
