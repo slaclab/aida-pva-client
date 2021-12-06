627d5e087a7964335821c587f850aa95b231ba88
george-mcintyre <george@level-n.com>
2021-12-06 14:14:06 +0100

    Remove ClientFactory.stop()

--- src/test/java/edu/stanford/slac/aida/test/AidaClientTest.java
+++ src/test/java/edu/stanford/slac/aida/test/AidaClientTest.java
@@ -16,57 +16,80 @@ import static org.junit.jupiter.api.Assertions.fail;
  */
 public class AidaClientTest {
     @Test
-    void test1() {
+    void testSimpleGet() {
         try {
+            System.out.println("#############################################");
             System.out.println("Test for request().returning(INTEGER).get() - Integer");
+            System.out.println("#############################################");
+
             Integer response = (Integer) request("XCOR:LI31:41:BCON")
                     .returning(INTEGER)
                     .get();
             assertEquals(16800, response);
+
             System.out.println("get: XCOR:LI31:41:BCON: returned: " + response);
+            System.out.println("_____________________________________________\n");
         } catch (RPCRequestException e) {
             fail(e.getMessage());
         }
     }
 
     @Test
-    void test2() {
+    void testGetRequest() {
         try {
+            System.out.println("#############################################");
             System.out.println("Test for getRequest() - Integer");
+            System.out.println("#############################################");
+
             Integer response = (Integer) getRequest("XCOR:LI31:41:BCON", INTEGER);
             assertEquals(16800, response);
+
             System.out.println("getRequest: XCOR:LI31:41:BCON: returned: " + response);
+            System.out.println("_____________________________________________\n");
         } catch (RPCRequestException e) {
             fail(e.getMessage());
         }
     }
 
     @Test
-    void test3() {
+    void testSimpleSet() {
         try {
+            System.out.println("#############################################");
             System.out.println("Test for request().set() - void");
+            System.out.println("#############################################");
+
             request("XCOR:LI31:41:BCON").set(5.0);
+
             System.out.println("set: XCOR:LI31:41:BCON = 5.0: returned: successfully");
+            System.out.println("_____________________________________________\n");
         } catch (RPCRequestException e) {
             fail(e.getMessage());
         }
     }
 
     @Test
-    void test4() {
+    void testSetRequest() {
         try {
+            System.out.println("#############################################");
             System.out.println("Test for setRequest() - void");
+            System.out.println("#############################################");
+
             setRequest("XCOR:LI31:41:BCON", 5.0);
+
             System.out.println("setRequest: XCOR:LI31:41:BCON = 5.0: returned: successfully");
+            System.out.println("_____________________________________________\n");
         } catch (RPCRequestException e) {
             fail(e.getMessage());
         }
     }
 
     @Test
-    void test5() {
+    void testGetTable() {
         try {
+            System.out.println("#############################################");
             System.out.println("Test for request().get() - AidaTable");
+            System.out.println("#############################################");
+
             AidaTable table = (AidaTable) request("KLYS:LI31:31:TACT")
                     .with("BEAM", 8)
                     .with("DGRP", "DEV_DGRP")
@@ -79,61 +102,86 @@ public class AidaClientTest {
             assertEquals(true, table.getValues().get("sleded").get(0));
             assertEquals(false, table.getValues().get("pampl").get(0));
             assertEquals(false, table.getValues().get("pphas").get(0));
+
             System.out.println("get: KLYS:LI31:31:TACT(BEAM=8,DGRP=DEV_DGRP): returned: " + table);
+            System.out.println("_____________________________________________\n");
         } catch (RPCRequestException e) {
             fail(e.getMessage());
         }
     }
 
     @Test
-    void test6() {
+    void testSetReturningTable() {
         try {
+            System.out.println("#############################################");
             System.out.println("Test for request().setReturningTable() - AidaTable");
+            System.out.println("#############################################");
+
             AidaTable table = request("KLYS:LI31:31:PDES")
                     .with("TRIM", "NO")
                     .setReturningTable(90.0f);
             assertEquals(0.0f, table.getValues().get("PHAS").get(0));
+
             System.out.println("set: KLYS:LI31:31:PDES = 0: returned: " + table);
+            System.out.println("_____________________________________________\n");
         } catch (RPCRequestException e) {
             fail(e.getMessage());
         }
     }
 
     @Test
-    void test7() {
+    void testGetErrors() {
         try {
+            System.out.println("#############################################");
             System.out.println("Test for get Errors");
+            System.out.println("#############################################");
+
             request("XCOR:LI31:4100:BCON")
                     .returning(FLOAT)
                     .get();
+
             fail("get: XCOR:LI31:4100:BCON: should have failed");
         } catch (RPCRequestException e) {
             assertEquals(" Unknown Unit requested; UnableToGetDataException; getting SLC db floating point device data", abbreviate(e.getMessage()));
-            System.out.println("get: XCOR:LI31:4100:BCON: failed as expected:" + abbreviate(e.getMessage()) );
+
+            System.out.println("get: XCOR:LI31:4100:BCON: failed as expected:" + abbreviate(e.getMessage()));
+            System.out.println("_____________________________________________\n");
         }
     }
 
     @Test
-    void test8() {
+    void testSetErrors() {
         try {
+            System.out.println("#############################################");
             System.out.println("Test for set Errors");
+            System.out.println("#############################################");
+
             setRequest("XCOR:LI31:41:BCON", "FOO");
+
             fail("setRequest: XCOR:LI31:41:BCON = FOO: should have failed");
         } catch (RPCRequestException e) {
             assertEquals("AidaInternalException; can't convert argument \"FOO\" to float", abbreviate(e.getMessage()));
-            System.out.println("setRequest: XCOR:LI31:41:BCON = FOO: failed as expected:" + abbreviate(e.getMessage()) );
+
+            System.out.println("setRequest: XCOR:LI31:41:BCON = FOO: failed as expected:" + abbreviate(e.getMessage()));
+            System.out.println("_____________________________________________\n");
         }
     }
 
+    /**
+     * Show an abbreviated version of the error message
+     *
+     * @param message the error message
+     * @return abbreviated version of the given message
+     */
     private String abbreviate(String message) {
         int end = message.indexOf(".");
         int endC = message.indexOf(", cause:");
-        if ( end == -1 ) {
+        if (end == -1) {
             return message;
         }
-        if ( endC == -1 ) {
+        if (endC == -1) {
             endC = end;
         }
-        return message.substring(0,Math.min(end, endC));
+        return message.substring(0, Math.min(end, endC));
     }
 }
